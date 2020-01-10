9cad17293af8822aeebf95c121da93a59a74da19
shonjiho <jihoson94@gmail.com>
2020-01-10 21:39:16 +0900

    modify JAEHASAFE

--- chapter20/JAEHASAFE/jiho.py
+++ chapter20/JAEHASAFE/jiho.py
@@ -1,6 +1,6 @@
 def get_partial_matched(n):
     pi = [0] * len(n)
-    
+
     begin = 1
     matched = 0
     while begin + matched < len(n):
@@ -14,10 +14,11 @@ def get_partial_matched(n):
                 begin += matched - pi[matched - 1]
                 matched = pi[matched - 1]
     return pi
-​
-def get_suffix_max_count(h, n):
+
+
+def get_common(h, n):
     pi = get_partial_matched(n)
-    
+
     begin = 0
     matched = 0
     while begin + matched < len(h):
@@ -32,27 +33,28 @@ def get_suffix_max_count(h, n):
                 begin += matched - pi[matched - 1]
                 matched = pi[matched - 1]
     return 0
-                
-​
+
+
 def solution(status):
     n = len(status)
-    
-    ret = 0 
+
+    ret = 0
     for i in range(n - 1):
-        if i % 2 == 0:
-            ret += get_suffix_count(status[i], status[i + 1])
+        clockwise = i % 2 == 0
+        if clockwise:
+            ret += get_common(status[i], status[i + 1])
         else:
-            ret += get_suffix_count(status[i + 1], status[i])
-    
+            ret += get_common(status[i + 1], status[i])
+
     return ret
-​
-​
+
+
 C = int(input())
-​
+
 for _ in range(C):
     N = int(input())
     status = []
-    for _ in range(N+1):
+    for _ in range(N + 1):
         status.append(input())
-    
+
     print(solution(status))
